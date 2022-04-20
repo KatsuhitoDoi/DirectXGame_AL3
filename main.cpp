f774ff690077c43f2e52701a6337a4758350298b
doi <doiktht@g.neec.ac.jp>
2022-04-20 12:08:50 +0900

    タイトルを英語表記に変更

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow("LE2D_00_ド_iAL3" );
+	win->CreateGameWindow("LE2D_00_doi_katsuhito_AL3" );
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
